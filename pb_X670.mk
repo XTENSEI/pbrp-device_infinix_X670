da5841e8900a9bb93fe3298f0f2492766d9c963e
JOHN ANDREI <andriegalutera@gmail.com>
2026-01-31 22:01:16 +0800

    Update and rename twrp_X670.mk to pb_X670.mk

--- pb_X670.mk
+++ pb_X670.mk
@@ -1,12 +1,10 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit the common TWRP product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit the base device-specific product configuration
 $(call inherit-product, device/infinix/X670/device.mk)
@@ -16,7 +14,7 @@ PRODUCT_BRAND := Infinix
 PRODUCT_DEVICE := X670
 PRODUCT_MANUFACTURER := infinix
 PRODUCT_MODEL := Infinix NOTE 12
-PRODUCT_NAME := twrp_X670
+PRODUCT_NAME := pb_X670
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
 
